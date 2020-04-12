9efd6ffbee5c47561ad33e576d1d479090634aca
Валерия <valeriasafargaleeva98@gmail.com>
2020-04-12 19:05:45 +0300

    first commit
    Signed-off-by: Валерия <valeriasafargaleeva98@gmail.com>

---
+++ main.py
@@ -0,0 +1,149 @@
+'''
+POS tag list
+CC coordinating conjunction
+CD cardinal digit
+DT determiner
+EX existential there (like: “there is” … think of it like “there exists”)
+FW foreign word
+IN preposition/subordinating conjunction
+JJ adjective ‘big’
+JJR adjective, comparative ‘bigger’
+JJS adjective, superlative ‘biggest’
+LS list marker 1)
+MD modal could, will
+NN noun, singular ‘desk’
+NNS noun plural ‘desks’
+NNP proper noun, singular ‘Harrison’
+NNPS proper noun, plural ‘Americans’
+PDT predeterminer ‘all the kids’
+POS possessive ending parent’s
+PRP personal pronoun I, he, she
+PRP$ possessive pronoun my, his, hers
+RB adverb very, silently,
+RBR adverb, comparative better
+RBS adverb, superlative best
+RP particle give up
+TO, to go ‘to’ the store.
+UH interjection, errrrrrrrm
+VB verb, base form take
+VBD verb, past tense took
+VBG verb, gerund/present participle taking
+VBN verb, past participle taken
+VBP verb, sing. present, non-3d take
+VBZ verb, 3rd person sing. present takes
+WDT wh-determiner which
+WP wh-pronoun who, what
+WP$ possessive wh-pronoun whose
+WRB wh-abverb where, when
+'''
+
+import pandas as pd
+import numpy as np
+from numpy import *
+import json
+import gensim
+from gensim import corpora
+from gensim.models import LsiModel
+from gensim.models.coherencemodel import CoherenceModel
+from gensim.utils import simple_preprocess
+import nltk
+from nltk.tokenize import word_tokenize, sent_tokenize
+from nltk.corpus import stopwords
+from string import punctuation
+from nltk.collocations import *
+from nltk.corpus import wordnet as wn
+from nltk.stem.lancaster import LancasterStemmer
+from nltk.corpus import brown
+from nltk.stem import SnowballStemmer
+from nltk.stem import WordNetLemmatizer
+from nltk.chunk import api
+from nltk.chunk.api import ChunkParserI
+from nltk import RegexpParser
+from nltk.tag import pos_tag
+from nltk import pos_tag_sents
+import scipy
+from scipy.spatial.distance import pdist
+from scipy.cluster import hierarchy
+from nltk.tokenize import PunktSentenceTokenizer
+from smart_open import smart_open
+import os
+import os.path
+from pprint import pprint
+from nltk.corpus import twitter_samples
+
+number_of_topics = 6
+words = 20
+stemmer = SnowballStemmer('english')
+
+
+def lemmatize_sentence(tokens):
+    lemmatizer = WordNetLemmatizer()
+    lemmatized_sentence = []
+    for word, tag in pos_tag(tokens):
+        if tag.startswith('NN'):
+            pos = 'n'
+        elif tag.startswith('VB'):
+            pos = 'v'
+        else:
+            pos = 'a'
+        lemmatized_sentence.append(lemmatizer.lemmatize(word, pos))
+    return lemmatized_sentence
+
+
+def get_word_applicant():
+    with open('trunc_Fashion.json', 'r') as f:
+        jsonData = json.load(f)
+
+    file_handler = open("Features.txt", "w")
+    dict1 = {}
+    for i in jsonData:
+        # if (i["overall"] > 1) and (i["overall"] < 5):
+        if (i["overall"] >= 1) and (i["overall"] <= 5):
+            tok_text = word_tokenize(i["reviewText"])
+            customStopWords = set(stopwords.words('english') + list(punctuation))
+            WordsStopResult = [word for word in tok_text if word not in customStopWords]
+            lemmitazer_output = lemmatize_sentence(tok_text)
+            pos_text = pos_tag(lemmitazer_output)
+            ChunkGramma = (r"Chunk:{(<NN>|<NNS>)+(<VB>|<VBD>|<MD>|<VBP>)?<DT>?(<NN>|<NNS>)} " "\n"
+                           r"{(<JJ>|<JJR>|<JJS>|<,>)+(<NN>|<NNS>)}" "\n"
+                           r"{<EX>(<JJ>|<JJR>|<JJS>)?((<NN>|<NNS>))?}" "\n"
+                           r"{<PRP>(<VB>|<VBD>|<MD>|<VBP>)<RB>?(<JJ>|<JJR>|<JJS>)+<CC>?(<JJ>|<JJR>|<JJS>|<NN>)?}" "\n"
+                           r" {<DT>(<VB>|<VBD>|<MD>|<VBP>)<DT>?(<RBS>|<RB>)?(<JJ>|<JJR>|<JJS>|<NN>|<,>)+}" "\n"
+                           r"{(<JJ>|<JJR>|<JJS>)+(<NN>|<NNS>)}" "\n"
+                           r"{(<JJ>|<JJR>|<JJS>|<,>)+(<NN>|<NNS>|<,>)+<CC>?<DT>?(<JJ>|<JJR>|<JJS>|<,>)+(<NN>|<NNS>|<,>)+}" "\n"
+                           r"{<PRP>(<VB>|<VBD>|<MD>|<VBP>)<RB>?<DT>?(<JJ>|<JJR>|<JJS>|<NN>|<,>)+}" "\n"
+                           r"{(<JJ>|<JJR>|<JJS>|<,>)+<CC>?(<JJ>|<JJR>|<JJS>|<,>)+}" "\n"
+                           r"{<PRP>(<VB>|<VBD>|<MD>|<VBP>)(<RB>|<RBR>|<RBS>|<,>)+}" "\n"
+                           r"{(<NN>|<NNS>|<,>)+<CC>?(<NN>|<NNS>|<,>)(<VB>|<VBD>|<MD>|<VBP>)?(<JJ>|<JJR>|<JJS>)+<CC>?(<JJ>|<JJR>|<JJS>|<NN>)?}" "\n"
+                           r"{(<JJ>|<JJR>|<JJS>|<RB>|<,>)+<IN><DT>(<NN>|<NNS>)<CC>?(<NN>|<NNS>)}" "\n"
+                           r"")
+            chunkParser = nltk.RegexpParser(ChunkGramma)
+            chunked = chunkParser.parse(pos_text)
+            # chunked.draw()
+
+            for subtree in chunked.subtrees():
+                if subtree.label() == 'Chunk':
+                    t = subtree
+                    t = ' '.join(word for word, pos in t.leaves())
+                    file_handler.write(str(t) + "\n")
+            dict1.update({i["id"]: pos_text})
+
+    file_handler.close()
+    return t
+
+
+def get_vector_applicant():
+    counter = 1
+    with open('Features.txt', 'r') as file_handler:
+        features_text = file_handler.readlines()
+    # print(features_text)
+    dict2 = {}
+    for i in features_text:
+        dict2.update({counter : i})
+        counter += 1
+
+    with open("FeaturesWithID.txt", "w") as featuresWithIdFile:
+        for key,value in dict2.items():
+            featuresWithIdFile.write("{}: {}\n".format(key,value))
+#get_word_applicant()
+get_vector_applicant()
