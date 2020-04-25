0ce9b26807f01763c98f00f9ca47cc53fa97627a
Валерия <valeriasafargaleeva98@gmail.com>
2020-04-25 19:03:16 +0300

    first commit
    Signed-off-by: Валерия <valeriasafargaleeva98@gmail.com>

--- main.py
+++ main.py
@@ -115,7 +115,7 @@ def lemmatize_sentence(tokens):
 
 
 def get_word_applicant():
-    with open('trunc_Fashion.json', 'r') as f:
+    with open('trunc_MusicInstrument.json', 'r') as f:
         jsonData = json.load(f)
 
     file_handler = open("Features.txt", "w")
@@ -372,7 +372,7 @@ def semantic_score(word1, word2):
 
 
 def NegativeWord():
-    with open('trunc_Fashion.json', 'r') as f:
+    with open('trunc_MusicInstrument.json', 'r') as f:
         jsonData = json.load(f)
 
     tag_negative_words = list(copy(jsonData))
@@ -394,7 +394,7 @@ def NegativeWord():
 
 
 def get_full_vector():
-    with open('trunc_Fashion.json', 'r') as f:
+    with open('trunc_MusicInstrument.json', 'r') as f:
         jsonData = json.load(f)
 
     corpus = []
@@ -566,7 +566,7 @@ def Forest_random():
 
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
     print(len(X_train), " train +", len(X_test), "test")
-
+    
     split = StratifiedShuffleSplit(n_splits=1, test_size=0.3, random_state=42)
     for train_index, test_index in split.split(X, y):
         # print("TRAIN:", train_index, "TEST:", test_index)
@@ -581,10 +581,76 @@ def Forest_random():
     print(confusion_matrix(y_test, model_predict))
     print(classification_report(y_test, model_predict))
 
+import stanza
+def tag_with_Stanford():
+    with open('trunc_MusicInstrument.json', 'r') as f:
+        jsonData = json.load(f)
 
-# get_word_applicant()
-# get_vector_applicant()
-# get_full_vector()
+    all_text_tag = []
+    list_reviews = []
+    for i in jsonData:
+        str_i = str(i["reviewText"])
+        list_reviews.append(str_i)
+    print(list_reviews)
+
+    total = []
+    for i in list_reviews:
+        nlp = stanza.Pipeline('en', processors='tokenize,pos,lemma', use_gpu=True, pos_batch_size=3000) # Build the pipeline, specify part-of-speech processor's batch size
+        doc = nlp(i) # Run the pipeline on the input text
+        wordDict = {}
+        for sentence in doc.sentences:
+            for word in sentence.words:
+                wordDict.update({"text":word.lemma,"xpos":word.xpos,"upos":word.upos})
+                all_text_tag.append(wordDict.copy())
+                wordDict.clear()
+        total.append(all_text_tag.copy())
+        all_text_tag.clear()
+
+    with open("Tag_list_Stanford.txt", "w") as tag_list:
+            json.dump(total, tag_list,indent=4)
+
+def deparse_name_group():
+    with open('trunc_MusicInstrument.json', 'r') as f:
+        jsonData = json.load(f)
+
+    list_reviews = []
+    for i in jsonData:
+        str_i = str(i["reviewText"])
+        list_reviews.append(str_i)
+
+    name_group = []
+    bag_of_list = []
+    for i in list_reviews:
+        nlp = stanza.Pipeline(lang='en', processors='tokenize,mwt,pos,lemma,depparse')
+        doc = nlp(i)
+        wordDict_name_group = {}
+        for sentence in doc.sentences:
+            for word in sentence.words:
+                wordDict_name_group.update({"word-1": word.text, "word-2": sentence.words[word.head-1].text, "deprel": word.deprel })
+                bag_of_list.append(wordDict_name_group.copy())
+                wordDict_name_group.clear()
+        for i in bag_of_list:
+            for key,value in i.items():
+                 if key == "deprel" and value == "compound":
+                    name_group.append(i.copy())
+
+    list_name_group = []
+    inter_list = []
+    for i in name_group:
+        for key,values in i.items():
+            if key == 'word-1' or key =='word-2':
+                inter_list.append(values)
+        list_name_group.append(inter_list.copy())
+        inter_list.clear()
+
+    with open("Name_group_Stanford.txt", "w") as name_group:
+            json.dump(list_name_group, name_group,indent=4)
+#get_word_applicant()
+#get_vector_applicant()
+#get_full_vector()
 # Logistic_Reression()
 #Decision_Tree()
-Forest_random()
+#Forest_random()
+#tag_with_Stanford()
+deparse_name_group()
+
